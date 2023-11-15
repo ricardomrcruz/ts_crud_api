cb8ad7ae72a770f0005360166fae8e7ec803759c
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2023-11-16 00:12:44 +0100

    sqlite_express_integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import sqlite3 from 'sqlite3';
+import { Ad } from '../types';
+
 const db = new sqlite3.Database('good_corner.sqlite');
-import { v4 as uuidv4 } from 'uuid';
 
 
 const app = express();
@@ -11,102 +12,144 @@ app.use(express.json());
 
 // json object
 
-let articles = [
-    {
-    id: "1",
-    title: "Neuroscience",
-    subtitle: "teh wonders of the brain",
-    intro: "I really like to blog about the sciences of the mind",
-    },
-    {
-    id: "2",
-    title: "wtvr",
-    subtitle: "teh wonders of the mind",
-    intro: "I really like this subject",
-    }
-    ];
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+  });
 
 // get route all articles
 
- app.get("/articles", (req, res) => {
+ app.get("/ads", (req: Request, res: Response) => {
     
-    // db.all("SELECT * FROM ad", (err, rows) => {
-    //     if (err) {
-    //         console.error(err.message);
-    //         res.status(500).send("Error occurred");
-    //         return;
-    //     }
-    //     res.send(rows);
-    // });
-    db.all("SELECT * FROM ad", (err, rows) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Error occurred");
-                return;
-            }
-            res.send(rows);
+        db.all("SELECT * FROM ad2", (err, rows:[Ad]) => {
+                if (!err) return res.send(rows);
+                console.log(err);
+                res.sendStatus(500);
         });
-
-
     //  console.log("tell me something");
-
     //  res.send(articles);
  });
 
 //  create route insert article
 
-app.post('/articles', (req, res) => {
+app.post('/ads', (req: Request, res: Response) => {
 
-    console.log(req.body);
+    // console.log(req.body);
+    
+    const newAd: Ad = {
+        ...req.body,
+        createdAt: new Date().toISOString(),
+    };
+
+    db.run(
+        "INSERT INTO ad2 (title, author, description, price, picture, location, createdAt, id_cat) VALUES ($title, $author, $description, $price, $picture, $location, $createdAt, $id_cat)",
+        {
+            $title: newAd.title,
+            $description: newAd.description,
+            $author: newAd.author,
+            $price: newAd.price,
+            $createdAt: newAd.createdAt,
+            $picture: newAd.picture,
+            $location: newAd.location,
+            $id_cat: newAd.id_cat,
+        },
+        function (this: any, err: any){
+            if(!err) return res.send({...newAd, id: this.lastID});
+            console.log(err);
+            res.sendStatus(500);
+            }
+    ); 
+    // ads.push(newAd);
+    // res.send("Request received, check the backend terminal");
+});
+
+// DELETE AD
+
+app.delete('/delete/:id', (req: Request, res: Response) => {
+    db.get("SELECT * FROM ad2 WHERE id=?", [req.params.id], (err, row) => {
+        if(err){
+            console.log(err);
+            res.sendStatus(500);
+        };
+    db.run("DELETE FROM ad2 where id=?", [req.params.id], (err: any) =>{
+        if(!err)return res.send(204);
     
 
-    const newAdd = {
-        id: articles.length + 1,
-        ...req.body
+        console.log(err);
+        res.sendStatus(500);
+     });
+    });
+    
+    // const { id } = req.params;
+
+    // articles = articles.filter(article => article.id.toString() !== id);
+
+    // res.send(`Article with the id ${id} got deleted!`);
+});
+
+// GET AD BY ID
+
+app.get("/ads/:id", (req: Request, res: Response) => {
+    db.get("SELECT * FROM ad2 WHERE id = ?", [req.params.id], (err, row) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(500);
       }
-      
-    articles.push(newAdd);
-    
-    res.send("Request received, check the backend terminal");
-
-});
-
-// delete article
-
-app.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-
-    
-    articles = articles.filter(article => article.id.toString() !== id);
-
-    res.send(`Article with the id ${id} got deleted!`);
-});
+      if (!row) return res.sendStatus(404);
+      res.send(row);
+    });
+  });
 
 
 // modify existing article by id 
 
-app.patch('/articles/:id', (req, res) => {
+app.patch('/ads/:id', (req: Request, res:Response) => {
+    
+    db.get("SELECT * FROM ad WHERE id = ?"), [req.params.id], (err, row) => {
+        if (err) {
+          console.log(err);
+          return res.sendStatus(500);
+        }
+        if (!row) return res.sendStatus(404);
+
+    // creates a string with this shape : "title = $title, description = $description, ..."
+    const setProps = Object.keys(req.body)
+      .reduce<string[]>((acc, prop) => [...acc, `${prop} = $${prop}`], [])
+      .join(", ");
+
+    // creates an object with this shape : {$title: "title sent by client", "$description: " description sent by client", ...}
+    const propsToUpdate = Object.keys(req.body).reduce(
+      (acc, prop) => ({ ...acc, [`$${prop}`]: req.body[prop] }),
+      {}
+    );
+
+    db.run(
+        `UPDATE ad SET ${setProps} WHERE id = $id`,
+        { ...propsToUpdate, $id: req.params.id },
+        (err: any) => {
+          if (!err) return res.send({ ...row, ...req.body });
+          console.log(err);
+          res.sendStatus(500);
+        }
+      );
+    
     
     // console.log("id of the ad to update", req.params.id);
 
-    console.log("props to update", req.body);
-    const { id } = req.params;
-    const {title, subtitle, intro} = req.body;
+    // console.log("props to update", req.body);
+    // const { id } = req.params;
+    // const {title, subtitle, intro} = req.body;
 
-
-    const article = articles.find((article)=>article.id.toString() === id);
+    // const article = articles.find((article)=>article.id.toString() === id);
     
-    if(article){
-      if(title) article.title = title;  
-      if(subtitle) article.title = title;  
-      if(intro) article.title = title;  
-    }
+    // if(article){
+    //   if(title) article.title = title;  
+    //   if(subtitle) article.title = title;  
+    //   if(intro) article.title = title;  
+    // }
     
+    // res.send(`article with the ${id} has been modified`);
 
-
-    res.send(`article with the ${id} has been modified`);
-
-
+    };
 });
 
 
---
+++ backend/types.ts
@@ -0,0 +1,11 @@
+export type Ad = {
+    id: number;
+    title: string;
+    description: string;
+    author: string;
+    price: number;
+    createdAt: string;
+    picture: string;
+    location: string;
+    id_cat: string;
+}
